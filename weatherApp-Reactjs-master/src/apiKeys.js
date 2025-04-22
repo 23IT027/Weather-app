9f21c2144f68731f7411cccf40e95998b1c9bf21
Donga Ayush Ketanbhai <23it027@charusat.edu.in>
2025-04-22 16:17:11 +0530

    removed sensetive info from apiKeys.js

--- weatherApp-Reactjs-master/src/apiKeys.js
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "ebad76d0037ddfd72e1c537ec6adb289",
+  key: "your key from openweather.com",
   base: "https://api.openweathermap.org/data/2.5/",
 };
